2f0fc7c02755db8a0d5499601577ffdfa98b49d0
chimdi ikechukwu <chimdiikechukwu00@gmail.com>
2025-02-21 03:36:57 +0100

    recent-update

--- news.js
+++ news.js
@@ -1,6 +1,6 @@
 const email = document.querySelector('#email');
+const emailConfirmation = document.getElementById('user-email');
 const errorMessage = document.getElementById('error');
-const userEmail = document.querySelector('input');
 const form = document.querySelector('form');
 const submit = document.getElementById('submit-btn');
 
@@ -10,4 +10,35 @@ email.addEventListener('input', () => {
   } else {
     errorMessage.style.display = 'none';
   }
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (email.value.includes('@') && email.value.includes('gmail.com')) {
+    setTimeout(() => {
+      window.location.href = `confirmation.html?email=${encodeURIComponent(
+        email.value
+      )}}`;
+    }, 100);
+  } else {
+    errorMessage.style.display = 'flex';
+  }
+});
+
+function getQueryParam(param) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+const userEmail = getQueryParam('email');
+if (emailConfirmation && userEmail) {
+  emailConfirmation.textContent = decodeURIComponent(userEmail);
+}
+
+const dismissConfirmation = document.getElementById('dismiss');
+
+dismissConfirmation.addEventListener('click', () => {
+  setTimeout(() => {
+    window.location.href = 'index.html';
+  }, 1000);
 });
